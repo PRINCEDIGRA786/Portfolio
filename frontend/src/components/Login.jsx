91bce0089ebeef3a7ced6c09a7b860eb74285483
Prince <121872553+PRINCEDIGRA786@users.noreply.github.com>
2024-02-21 23:15:31 +0530

    Update Login.jsx

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -17,7 +17,7 @@ const Login = () => {
 
   const handleLogin =async (e) => {
     e.preventDefault();
-    const response = await fetch("http://localhost:5000/api/auth/login", {
+    const response = await fetch("https://portfolio-princedigra786.vercel.app/api/auth/login", {
       method: "POST", // *GET, POST, PUT, DELETE, etc.
       headers: {
           "Content-Type": "application/json",
