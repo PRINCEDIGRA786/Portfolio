35a3608cc8c372557507fc64ba529e7e112c62db
Prince <121872553+PRINCEDIGRA786@users.noreply.github.com>
2024-02-21 23:13:55 +0530

    Update PortState.js

--- frontend/src/contextapi/PortState.js
+++ frontend/src/contextapi/PortState.js
@@ -4,7 +4,7 @@ import Portcon from './Portfoliocontext'
 export default function PortState(props) {
     const [user, setuser] = useState({})
     const [name, setname] = useState("")
-    const host = "http://localhost:5000/api/folio"
+    const host = "https://portfolio-princedigra786.vercel.app"
     const createPortfolio = async (name, email, image, about, profession, hobbies, education, experience
     ) => {
         const pic = await image;
@@ -12,7 +12,7 @@ export default function PortState(props) {
         await experience.pop();
         await education.pop();
 
-        const response = await fetch(`${host}/createportfolio`, {
+        const response = await fetch(`${host}/api/folio/createportfolio`, {
             method: "POST", // *GET, POST, PUT, DELETE, etc.
             headers: {
                 "Content-Type": "application/json",
@@ -37,7 +37,7 @@ export default function PortState(props) {
     }
 
     const dashboard = async () => {
-        const response = await fetch(`${host}/dashboard`, {
+        const response = await fetch(`${host}/api/folio/dashboard`, {
             method: "GET", // *GET, POST, PUT, DELETE, etc.
             headers: {
                 "Content-Type": "application/json",
@@ -50,7 +50,7 @@ export default function PortState(props) {
 
     }
     const getuser = async () => {
-        const response = await fetch("http://localhost:5000/api/auth/getuser", {
+        const response = await fetch(`${host}/api/auth/getuser`, {
             method: "POST", // *GET, POST, PUT, DELETE, etc.
             headers: {
                 "Content-Type": "application/json",
@@ -66,7 +66,7 @@ export default function PortState(props) {
 
     const [allusers, setallusers] = useState([])
     const fetchUsers = async () => {
-        const response = await fetch("http://localhost:5000/api/folio/fetchall", {
+        const response = await fetch(`${host}/api/folio/fetchall`, {
             method: "GET", // *GET, POST, PUT, DELETE, etc.
             headers: {
                 "Content-Type": "application/json",
@@ -79,7 +79,7 @@ export default function PortState(props) {
 
     }
     const documentAdd = async (documents) => {
-        const response = await fetch("http://localhost:5000/api/folio/adddocuments", {
+        const response = await fetch(`${host}/api/folio/adddocuments`, {
             method: "PUT", // *GET, POST, PUT, DELETE, etc.
             headers: {
                 "Content-Type": "application/json",
@@ -99,29 +99,13 @@ export default function PortState(props) {
 
     }
 
-    // This is for creating the user to the dummy.io for the post and comments secitons:
-    const dumcreateuser = async () => {
-
-        const response = await fetch("https://dummyapi.io/data/v1/user/create", {
-            method: "POST", // *GET, POST, PUT, DELETE, etc.
-            headers: {
-                "app-id": "65d4d43d786900618066e176"
-            },
-            // mode: "navigate",
-            body: { firstName: "prince", lastName: "digra", email: "digrapirnce9@gmail.com" }
-        });
-        console.log("The response is:", response)
-        // const json = await response.json();
-        console.log("The json response is:", response.lastName)
-    }
-
-
 
     return (
         <>
-            <Portcon.Provider value={{ createPortfolio, dumcreateuser, documentAdd, getuser, name, fetchUsers, allusers, user, dashboard }}>
+            <Portcon.Provider value={{ createPortfolio, documentAdd, getuser, name, fetchUsers, allusers, user, dashboard }}>
                 {props.children}
             </Portcon.Provider>
         </>
     )
 }
+
